1a25c6a23fb0683cffee4ed358baea7e8bb85350
arminv <arminv@users.noreply.github.com>
2020-12-31 17:45:27 -0500

    prgoress

--- client/dist/src.a2b27638.js
+++ client/dist/src.a2b27638.js
@@ -54593,7 +54593,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61290" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65198" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
