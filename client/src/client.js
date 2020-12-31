6c5bd88a032c0e4889ce85f87be5a05bb3c74f78
arminv <arminv@users.noreply.github.com>
2020-12-31 16:48:20 -0500

    First commit

---
+++ client/src/client.js
@@ -0,0 +1,8 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import gql from 'graphql-tag'
+
+/**
+ * Create a new apollo client and export as default
+ */
